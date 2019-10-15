f5d11ea015d0ce636bc0e858f6bd7a18a37f8a37
Brian MacPherson <brimac1634@gmail.com>
2019-10-15 14:24:09 +0800

    added checking of stock on adding to cart

--- src/components/product-details/product-details.component.js
+++ src/components/product-details/product-details.component.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { 
@@ -10,8 +10,6 @@ import {
 
 import { selectProduct } from '../../redux/shop/shop.selectors';
 import { addItemStart } from '../../redux/cart/cart.actions';
-import { setAlert } from '../../redux/alert/alert.actions'; 
-import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
@@ -25,151 +23,132 @@ import './product-details.styles.scss';
 
 const mapStateToProps = (state, ownProps) => ({
 	product: selectProduct(ownProps.match.params.productID)(state),
-	cartItems: selectCartItems(state)
+	cartError: state.cart.error,
 })
 
 const mapDispatchToProps = dispatch => ({
-	addItem: item => dispatch(addItemStart(item)),
-	setAlert: message => dispatch(setAlert(message))
+	addItem: item => dispatch(addItemStart(item))
 })
 
-class ProductDetails extends Component {
-	state = {
-		currentImage: this.props.product.images[0],
-		quantity: 1
+const ProductDetails = ({ product, addItem, cartItems }) => {
+	const { name, images, price, description, features } = product;
+	const [currentImage, setImage] = useState(images[0]);
+	const [quantity, setQuantity] = useState(1);
+
+	const handleChange = e => {
+		const { value } = e.target;
+		setQuantity(Number(value));
 	}
 
-	componentDidMount() { 
-		setTimeout(()=>this.setState({fade: null}), 600) 
-	}
-
-	handleChange = e => {
-		const { value, name } = e.target;
-		this.setState({ [name]: Number(value)});
-	}
-
-	addItemToCart = (item, quantity) => {
-		const { addItem, setAlert, cartItems } = this.props;
+	const addItemToCart = (item, quantity) => {
 		addItem({ item, quantity, cartItems });
-		setAlert('Added to Cart');
 	}
 
-	render() {
-		const { currentImage, quantity } = this.state;
-		const { 
-			product, 
-			product: { name, images, price, description, features } 
-		} = this.props;
-		const quantityAvailable = product.quantity;
+	const quantityAvailable = product.quantity;
 
-		const currentURL = window.location.href
+	const currentURL = window.location.href
 
-		return (
-			<div className='product-details'>
-				<div className='panels'>
-					<div className='panel'>
-						<HoverZoom 
-							src={currentImage} 
-							alt='product image' 
-						/>
-						<div className='image-list'>
-							{
-								images &&
-								images.map((image, i) => (
-									<div 
-										key={i}
-										className='list-image' 
-										style={{backgroundImage: `url(${image})`}} 
-										onClick={()=>this.setState({currentImage: image})}
-									/>
-								))
-							}
-						</div>
-					</div>
-					<div className='panel'>
-						<h1>{ name }</h1>
-						<span className='price'>{ `HKD$${price}` }</span>
+	return (
+		<div className='product-details'>
+			<div className='panels'>
+				<div className='panel'>
+					<HoverZoom 
+						src={currentImage} 
+						alt='product image' 
+					/>
+					<div className='image-list'>
 						{
-							quantityAvailable >= 1
-							? 	<div className='order'>
-									<FormInput 
-										name='quantity' 
-										type='number'
-										min={1}
-										max={quantityAvailable}
-										value={quantity} 
-										label='Quantity'
-										handleChange={this.handleChange}
-									/>
-									{
-										quantityAvailable === 1 &&
-										<span className='one-left'>Only 1 left in stock!</span>
-									}
-									<CustomButton 
-										onClick={()=>this.addItemToCart(product, quantity)}
-									> 
-										Add to Cart
-									</CustomButton>
-								</div>
-							: 	<span className='order not-avail'>Out of Stock</span>
-						}
-						
-						<p className='description'>{ description }</p>
-						{
-							features &&
-							features.map((feature, i) => (
-								<div key={i} className='feature'>
-									<span >&#8226;</span>
-									<span>{ feature }</span>
-								</div>
+							images &&
+							images.map((image, i) => (
+								<div 
+									key={i}
+									className='list-image' 
+									style={{backgroundImage: `url(${image})`}} 
+									onClick={()=>setImage(image)}
+								/>
 							))
 						}
-						<div className='share'>
-							<span>Share:</span>
-							<div className='share-buttons'>
-								<FacebookShareButton 
-									className='share-btn'
-									url={currentURL} 
-									hashtag='#bonvivantcollection'
-								>
-									<div className='icon'>
-										<FacebookIcon />
-									</div>
-								</FacebookShareButton>
-								<TwitterShareButton 
-									className='share-btn'
-									url={currentURL}
-									hashtags={['bonvivantcollection']} 
-								>
-									<div className='icon'>
-										<TwitterIcon />
-									</div>
-								</TwitterShareButton>
-								<WhatsappShareButton 
-									className='share-btn'
-									url={currentURL}
-									title='Bon Vivant Collection'
-								>
-									<div className='icon'>
-										<WhatsappIcon />
-									</div>
-								</WhatsappShareButton>
-								<EmailShareButton 
-									className='share-btn'
-									url={currentURL}
-									subject={`Bon Vivant Collection - ${name}`}
-								>
-									<div className='icon'>
-										<EmailIcon />
-									</div>
-								</EmailShareButton>
+					</div>
+				</div>
+				<div className='panel'>
+					<h1>{ name }</h1>
+					<span className='price'>{ `HKD$${price}` }</span>
+					{
+						quantityAvailable >= 1
+						? 	<div className='order'>
+								<FormInput 
+									name='quantity' 
+									type='number'
+									min={1}
+									max={quantityAvailable}
+									value={quantity} 
+									label='Quantity'
+									handleChange={handleChange}
+								/>
+								<CustomButton 
+									onClick={()=>addItemToCart(product, quantity)}
+								> 
+									Add to Cart
+								</CustomButton>
 							</div>
+						: 	<span className='order not-avail'>Out of Stock</span>
+					}
+					
+					<p className='description'>{ description }</p>
+					{
+						features &&
+						features.map((feature, i) => (
+							<div key={i} className='feature'>
+								<span >&#8226;</span>
+								<span>{ feature }</span>
+							</div>
+						))
+					}
+					<div className='share'>
+						<span>Share:</span>
+						<div className='share-buttons'>
+							<FacebookShareButton 
+								className='share-btn'
+								url={currentURL} 
+								hashtag='#bonvivantcollection'
+							>
+								<div className='icon'>
+									<FacebookIcon />
+								</div>
+							</FacebookShareButton>
+							<TwitterShareButton 
+								className='share-btn'
+								url={currentURL}
+								hashtags={['bonvivantcollection']} 
+							>
+								<div className='icon'>
+									<TwitterIcon />
+								</div>
+							</TwitterShareButton>
+							<WhatsappShareButton 
+								className='share-btn'
+								url={currentURL}
+								title='Bon Vivant Collection'
+							>
+								<div className='icon'>
+									<WhatsappIcon />
+								</div>
+							</WhatsappShareButton>
+							<EmailShareButton 
+								className='share-btn'
+								url={currentURL}
+								subject={`Bon Vivant Collection - ${name}`}
+							>
+								<div className='icon'>
+									<EmailIcon />
+								</div>
+							</EmailShareButton>
 						</div>
 					</div>
 				</div>
 			</div>
-		)
-	}
+		</div>
+	)
 }
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ProductDetails));
--- src/redux/cart/cart.sagas.js
+++ src/redux/cart/cart.sagas.js
@@ -1,18 +1,29 @@
-import { all, call, put, takeLatest } from 'redux-saga/effects';
+import { all, call, put, takeLatest, select } from 'redux-saga/effects';
 import axios from 'axios';
 
 import CartActionTypes from '../cart/cart.types';
 import { addItemSuccess, addItemFailure } from './cart.actions';
+import { setAlert } from '../alert/alert.actions';
+
+const getCartItems = state => state.cart.cartItems;
 
 export function* addItem({payload}) {
-	const { item, quantity, cartItems } = payload;
+	const { item, quantity } = payload;
 	try {
+		const cartItems = yield select(getCartItems);
 		const { data } = yield axios.post('/get-product-quantity', { productIDs: [payload.item.product_id] })
+		const totalQuantityInStock = data[0].quantity
 		const itemInCart = cartItems.filter(cartItem => cartItem.product_id === item.product_id)[0];
 		const existingQuantity = itemInCart ? itemInCart.quantity : 0;
-		payload.quantity + existingQuantity <= data[0].quantity
-			?	yield put(addItemSuccess({ item, quantity }))
-			: 	yield put(addItemFailure('Not enough in stock'))
+		payload.quantity + existingQuantity <= totalQuantityInStock
+			?	yield all([
+					put(addItemSuccess({ item, quantity })),
+					put(setAlert('Added to Cart'))
+				])
+			: 	yield all([
+					put(addItemFailure('not enough in stock')),
+					put(setAlert(`Sorry, we only have ${totalQuantityInStock} of these in stock.`))
+				])
 	} catch (err) {
 		yield put(addItemFailure(err))
 	}
